d6c1aebd1a9e49b53aa403012b847d360c5c334b
trevsm <trevorsm@pm.me>
2021-04-18 16:13:29 -0600

    Added files

---
+++ scripts.js
@@ -0,0 +1,39 @@
+// react createElement
+function createElement(tagName, attrs = {}, ...children) {
+  const elem = Object.assign(document.createElement(tagName), attrs);
+  for (const child of children) {
+    if (Array.isArray(child)) elem.append(...child);
+    else elem.append(child);
+  }
+  return elem;
+}
+
+async function getProjects() {
+  const data = await fetch("./projects.json").then((r) => r.json());
+  return data;
+}
+
+const cardElement = ({ title, description, languages, links }) =>
+  createElement("div", { className: "card" }, [
+    createElement("h3", {}, [
+      links.live ? createElement("a", { href: links.live }, title) : title,
+    ]),
+    createElement("div", { className: "description" }, description),
+    createElement("div", { className: "languages" }, [
+      ...languages.map((e) => createElement("div", { className: "item" }, e)),
+    ]),
+    createElement("div", { className: "links" }, [
+      links.source ? createElement("a", { href: links.source }, "Source") : "",
+    ]),
+  ]);
+
+function main() {
+  const project_list = document.querySelector(".project_list");
+  getProjects().then((p) => {
+    p.forEach((e) => {
+      project_list.appendChild(cardElement(e));
+    });
+  });
+}
+
+main();
